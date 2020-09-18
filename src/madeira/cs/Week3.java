e9626fbf3e1c5e81969e7f0ff5da9f14fa255dd8
Mr. Wallace <pwallace@madeira.org>
2020-09-18 10:30:17 -0400

    returning graded work

--- src/madeira/cs/Week3.java
+++ src/madeira/cs/Week3.java
@@ -17,8 +17,9 @@ public class Week3 {
 
         if (firstNumber < secondNumber)
             System.out.print("The second number is greater");
-        if (firstNumber > secondNumber)
+        if (firstNumber > secondNumber) // you need curly braces here
             System.out.print("The first number is greater");
+        // you should put an else if the numbers are equal
 
 
         //Problem II
@@ -29,6 +30,9 @@ public class Week3 {
 
         if (favorite.equals("Alternative")) {
             System.out.println("Same!");
+            // So here you'll just need a plan else!
+            // This won't print Lame b/c what you're comparing is the string to a boolean
+            // which will always be false 
         } else if (favorite.equals( aa || aa)) {
             System.out.println("Lame");
         }
@@ -42,23 +46,25 @@ public class Week3 {
 
         System.out.println("Try to guess my number!:");
 
-        while (true) {
+        while (true) { // it would be best to say (count <= guess) so that it will run 10 times
             guess = input3.nextInt();
             count++;
 
             if (guess == a) {
                 System.out.println("Your guess is correct. Congratulations!");
             } else if (count < 9) {
+                // so this will print even if the user has only guessed once
                 System.out.println("Wow no you're just wrong. I gave you 10 tries too. Oh well, better luck next time= oh wait you don't have another chance, do you? That's what I thought.");
             }
+
         }
 
 
-        // Problem IV
-        String repeat = new String(new char[5]).replace("*");
+        // // Problem IV
+        // String repeat = new String(new char[5]).replace("*");
 
-        // Problem V
-        String repeated = new String(new char[*2]).repeated.replace("*");
+        // // Problem V
+        // String repeated = new String(new char[2]).repeated.replace("*");
 
     }
 }
